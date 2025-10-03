2cccf0f7987b67a65efbd55db68d92eca86d569b
Yurikzzz <jakub.hess.2008@gmail.com>
2025-10-04 00:15:27 +0200

    Vylepseni dveri (otevirani a zavirani, otevreny a zavreny sprite, automaticke zavirani po urcitem case)

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,9 +1,67 @@
 using UnityEngine;
+using System.Collections;
 
 public class Door : Interactable
 {
+    [SerializeField] private Collider2D blockingCollider;
+    [SerializeField] private Sprite closedSprite;
+    [SerializeField] private Sprite openSprite;
+    private SpriteRenderer spriteRenderer;
+
+    private bool isOpen = false;
+    [SerializeField] private float autoCloseDelay = 3f; 
+
+    private Coroutine autoCloseRoutine;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = closedSprite;
+    }
+
     public override void Interact()
     {
-        Debug.Log("You have interacted with the door.");
+        if (!isOpen)
+        {
+            OpenDoor();
+        }
+        else
+        {
+            CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        isOpen = true;
+        blockingCollider.enabled = false;
+        spriteRenderer.sprite = openSprite;
+        Debug.Log("Door opened");
+
+        if (autoCloseRoutine != null)
+        {
+            StopCoroutine(autoCloseRoutine);
+        }
+        autoCloseRoutine = StartCoroutine(AutoCloseAfterDelay());
+    }
+
+    private void CloseDoor()
+    {
+        isOpen = false;
+        blockingCollider.enabled = true;
+        spriteRenderer.sprite = closedSprite;
+        Debug.Log("Door closed");
+
+        if (autoCloseRoutine != null)
+        {
+            StopCoroutine(autoCloseRoutine);
+            autoCloseRoutine = null;
+        }
+    }
+
+    private IEnumerator AutoCloseAfterDelay()
+    {
+        yield return new WaitForSeconds(autoCloseDelay);
+        CloseDoor();
     }
 }
--- Assets/Scripts/PlayerInteractor.cs
+++ Assets/Scripts/PlayerInteractor.cs
@@ -4,7 +4,6 @@ public class PlayerInteractor : MonoBehaviour
 {
     public float interactRange = 2f;
     public LayerMask interactableMask;
-    public GameObject ePrompt;
 
     private Interactable currentTarget;
     private Interactable lastTarget;
