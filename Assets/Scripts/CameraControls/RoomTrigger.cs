e13b8f27d271c562e1dab3571c870ea50e21846b
Yurikzzz <jakub.hess.2008@gmail.com>
2025-11-02 14:19:37 +0100

    Upraveni graficke stranky hry a fixnuti kamery

--- Assets/Scripts/CameraControls/RoomTrigger.cs
+++ Assets/Scripts/CameraControls/RoomTrigger.cs
@@ -4,13 +4,14 @@ public class RoomTrigger : MonoBehaviour
 {
     [SerializeField]
     public float targetZoom;
+    public Transform focus;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             var camController = Camera.main.GetComponent<RoomCameraController>();
-            camController.MoveToRoom(transform, targetZoom);
+            camController.MoveToRoom(focus, targetZoom);
         }
     }
 }
