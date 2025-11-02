1f5927e13c42411ec4032cc3f839e0fa4aa0bcec
Yurikzzz <jakub.hess.2008@gmail.com>
2025-11-02 17:30:30 +0100

    Pridani player modelu a jeho animaci

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,32 +5,39 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed = 3f; 
     public float runSpeed = 6f;  
 
+    public Animator animator;
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+
     private float moveInput;
     private float currentSpeed;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         moveInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
-        {
-            currentSpeed = runSpeed;
-        }
-        else
-        {
-            currentSpeed = walkSpeed;
-        }
+        bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+        if (moveInput > 0)
+            spriteRenderer.flipX = true;
+        else if (moveInput < 0)
+            spriteRenderer.flipX = false;
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput * currentSpeed)); 
+        animator.SetBool("isRunning", isRunning);
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + new Vector2(moveInput, 0f) * currentSpeed * Time.fixedDeltaTime);
+        Vector2 newPosition = rb.position + new Vector2(moveInput, 0f) * currentSpeed * Time.fixedDeltaTime;
+        rb.MovePosition(newPosition);
     }
 }
 
