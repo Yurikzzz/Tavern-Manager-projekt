9c1372981bca4280c3b77e0e50a7c8e2f3aec417
Yurikzzz <jakub.hess.2008@gmail.com>
2025-11-18 09:52:03 +0100

    Improved bar UI and added confirmation to opening/closing the tavern

--- Assets/Scripts/InteractionSystem/TavernDoor.cs
+++ Assets/Scripts/InteractionSystem/TavernDoor.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TavernDoor : Interactable
 {
     [SerializeField] private Sprite closedSprite;
     [SerializeField] private Sprite openSprite;
+    [Header("Confirmation UI")]
+    [SerializeField] private GameObject confirmationPanel;
+    [SerializeField] private Text confirmationLabel;
+    [SerializeField] private Button confirmButton;
+    [SerializeField] private Button cancelButton;
+
     private SpriteRenderer spriteRenderer;
     private bool isOpen = false;
+    private enum PendingAction { None, Open, Close }
+    private PendingAction pendingAction = PendingAction.None;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = closedSprite;
+
+        if (confirmationPanel != null)
+        {
+            confirmationPanel.SetActive(false);
+        }
+
+        if (confirmButton != null)
+        {
+            confirmButton.onClick.AddListener(ConfirmAction);
+        }
+
+        if (cancelButton != null)
+        {
+            cancelButton.onClick.AddListener(CancelAction);
+        }
     }
 
     public override void Interact()
@@ -20,18 +44,69 @@ public class TavernDoor : Interactable
         switch (timeManager.CurrentTime)
         {
             case GameTimeManager.TimeOfDay.Morning:
-                OpenTavern();
-                timeManager.SetTime(GameTimeManager.TimeOfDay.Afternoon);
+                ShowConfirmation(PendingAction.Open);
                 break;
 
             case GameTimeManager.TimeOfDay.Afternoon:
-                CloseTavern();
-                timeManager.SetTime(GameTimeManager.TimeOfDay.Night);
+                ShowConfirmation(PendingAction.Close);
                 break;
 
             default:
                 Debug.Log("You can only open in the morning or close in the evening.");
                 break;
+        }
+    }
+
+    private void ShowConfirmation(PendingAction action)
+    {
+        if (confirmationPanel == null || confirmationLabel == null)
+        {
+            Debug.LogWarning("Confirmation UI has not been assigned on the TavernDoor.");
+            return;
+        }
+
+        pendingAction = action;
+        confirmationPanel.SetActive(true);
+        confirmationLabel.text = action == PendingAction.Open
+            ? "Are you sure you want to open the tavern?"
+            : "Are you sure you want to close the tavern?";
+    }
+
+    private void ConfirmAction()
+    {
+        if (pendingAction == PendingAction.None)
+        {
+            return;
+        }
+
+        var timeManager = GameTimeManager.Instance;
+
+        if (pendingAction == PendingAction.Open)
+        {
+            OpenTavern();
+            timeManager.SetTime(GameTimeManager.TimeOfDay.Afternoon);
+        }
+        else if (pendingAction == PendingAction.Close)
+        {
+            CloseTavern();
+            timeManager.SetTime(GameTimeManager.TimeOfDay.Night);
+        }
+
+        HideConfirmation();
+    }
+
+    private void CancelAction()
+    {
+        HideConfirmation();
+    }
+
+    private void HideConfirmation()
+    {
+        pendingAction = PendingAction.None;
+
+        if (confirmationPanel != null)
+        {
+            confirmationPanel.SetActive(false);
         }
     }
 
@@ -41,7 +116,6 @@ public class TavernDoor : Interactable
         isOpen = true;
         spriteRenderer.sprite = openSprite;
         Debug.Log("The tavern is now open for business!");
-        // later: enable NPC spawner or door collider
     }
 
     private void CloseTavern()
@@ -50,6 +124,5 @@ public class TavernDoor : Interactable
         isOpen = false;
         spriteRenderer.sprite = closedSprite;
         Debug.Log("The tavern is now closed for the night!");
-        // later: disable NPC spawner or door collider
     }
 }
